6a3d7141365a0be324958b4313ed0566203f6b29
elyeskabous <elyes.kabous@gmail.com>
2023-02-28 16:40:08 +0100

    cityFlat backend : paypal integration 1.4 : backend v0.0.1

--- src/routes/paypal.routes.js
+++ src/routes/paypal.routes.js
@@ -6,100 +6,102 @@ const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
     'client_id': PAYPAL_CLIENT_ID,
-    'client_secret':PAYPAL_APP_SECRET
-  });
-  
+    'client_secret': PAYPAL_APP_SECRET
+});
+
 /** Defining the router */
 const paypalRouter = express.Router();
 
 
-   //*********paypal payment routes*******// 
- let amount =0;
+//*********paypal payment routes*******// 
+let amount = 0;
 
- paypalRouter
- .get("/success", (req, res) =>{
+paypalRouter
+    .get("/success", (req, res) => {
 
-   var execute_payment_json = {
-      "payer_id": req.query.PayerID,
-      "transactions": [{
-          "amount": {
-              "currency": "USD",
-              "total": "150.0"
-          }
-      }]
-   };
-   
-   var paymentId = req.query.paymentId ;
-   
-   paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-      if (error) {
-          console.log(error.response);
-          throw error;
-      } else {
-          console.log("Get Payment Response (success!!)");
-        
-          console.log(JSON.stringify(payment));
-    
-      }
-   });
-   
+        var execute_payment_json = {
+            "payer_id": req.query.PayerID,
+            "transactions": [{
+                "amount": {
+                    "currency": "USD",
+                    "total": "150.0"
+                }
+            }]
+        };
+
+        var paymentId = req.query.paymentId;
+
+        paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+            if (error) {
+                console.log(error.response);
+                throw error;
+            } else {
+             res.status(200).json({message:"success payment !"});
+
+                console.log(JSON.stringify(payment));
+
+            }
+        });
+    });
+
+    paypalRouter
+    .get("/cancel", (req, res) => {
+
+       
+    });
+
+paypalRouter
+    .get('/pay', async (req, res) => {
+
+        amount = req.body.price;
+
+        var create_payment_json = {
+            "intent": "sale",
+            "payer": {
+                "payment_method": "paypal"
+            },
+            "redirect_urls": {
+                "return_url": "http://127.0.0.1:9090/paypal/success",
+                "cancel_url": "http://127.0.0.1:9090/paypal/cancel"
+            },
+            "transactions": [{
+                "item_list": {
+                    "items": [{
+                        "name": "item",
+                        "sku": "item",
+                        "price": "150.0",
+                        "currency": "USD",
+                        "quantity": 1
+                    }]
+                },
+                "amount": {
+                    "currency": "USD",
+                    "total": "150.0"
+                },
+                "description": "This is the payment description."
+            }]
+        };
 
 
+        paypal.payment.create(create_payment_json, (error, payment) => {
+            if (error) {
+                throw error;
+            } else {
+                console.log("Create Payment Response");
+                console.log(payment);
+                for (var index = 0; index < payment.links.length; index++) {
+                    //Redirect user to this endpoint for redirect url
+                    if (payment.links[index].rel === 'approval_url') {
 
- });
 
- paypalRouter
-.get('/pay',async (req, res) => {
-   
-   amount = req.body.price;
+                        res.redirect(payment.links[index].href);
+                        console.log("Redirected to approval_url successfully");
 
-   var create_payment_json = {
-      "intent": "sale",
-      "payer": {
-          "payment_method": "paypal"
-      },
-      "redirect_urls": {
-          "return_url":"http://127.0.0.1:9090/paypal/success",
-          "cancel_url": "http://cancel.url"
-      },
-      "transactions": [{
-          "item_list": {
-              "items": [{
-                  "name": "item",
-                  "sku": "item",
-                  "price": "150.0",
-                  "currency": "USD",
-                  "quantity": 1
-              }]
-          },
-          "amount": {
-              "currency": "USD",
-              "total":"150.0"
-          },
-          "description": "This is the payment description."
-      }]
-  };
-  
-  
-  paypal.payment.create(create_payment_json,  (error, payment)=> {
-      if (error) {
-          throw error;
-      } else {
-          console.log("Create Payment Response");
-          console.log(payment);
-          for (var index = 0; index < payment.links.length; index++) {
-            //Redirect user to this endpoint for redirect url
-                if (payment.links[index].rel === 'approval_url') {
-                    
-                 
-                     res.redirect(payment.links[index].href);
-                     console.log("Redirected to approval_url successfully");
-                   
+                    }
                 }
             }
-      }
-  });
- });
+        });
+    });
 
 
- export { paypalRouter };
+export { paypalRouter };
