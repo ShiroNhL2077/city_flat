edd869a2ba1485eb463456615d92a88dae939df8
wafa <63404321+Oueslati-Waafa@users.noreply.github.com>
2023-03-27 15:18:26 +0100

    luxury apartments fetch

--- frontend/src/components/Luxury/LuxuriousCollection/luxuriousCollection.js
+++ frontend/src/components/Luxury/LuxuriousCollection/luxuriousCollection.js
@@ -1,10 +1,10 @@
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import axios from 'axios';
-import React, { useState } from 'react'
-import { useEffect } from 'react';
-import Rate from '../../Rate/Rate';
-import './luxuriouCollection.css'
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Rate from "../../Rate/Rate";
+import "./luxuriouCollection.css";
 
 function LuxuriousCollection() {
   const [rating1, setRating1] = useState(0);
@@ -16,17 +16,23 @@ function LuxuriousCollection() {
 
   const [apartments, setApartments] = useState([]);
 
+  /*useEffect(() =>{
+    fetch('http://localhost:9090/appartments/getAllAppart')
+    .then(resp => resp.json())
+    .then((data) => setApartments(data))
+    .catch ((error) => console.log({error}));
+  },[]);*/
+
   useEffect(() => {
     axios
       .get("http://localhost:9090/appartments/getAllAppart")
       .then((result) => {
-        console.log(result.data);
         setApartments(result.data);
+        console.log();
+        
       })
       .catch((error) => console.log(error));
   }, []);
-
-
 
   return (
     <section className="luxury__collection__page">
@@ -35,49 +41,47 @@ function LuxuriousCollection() {
         <div className="line-in-middle"></div>
       </div>
       <div className="luxury_collection_items_content">
-      <div className="luxury_collection_content">
-        <div className="row">
-        {apartments.map((data) => {
-          return(
-          <div className="col-sm-6" key={data.id}>
-            {" "}
-            <div className="card">
-              <div className="card_img">
-                <img
-                  src="./luxury-apartments.png"
-                  className="card-img-top"
-                  alt="..."
-                />
-              </div>
-              <div className="card_body">
-                <div className="like_button_luxury">
-                  <button>
-                    <FontAwesomeIcon
-                      icon={faHeart}
-                      beat
-                      className="highlight_luxury"
-                    />
-                  </button>
-                </div>
-                <div className="card_content_luxury">
-                  <h3>{data.name}</h3>
-                  <p>{data.description}</p>
-                  <Rate
-                    rating={rating2}
-                    onRating={(rate) => setRating2(rate)}
-                  />
-                  <strong>{data.pricePerNight}€</strong>
-                  <strong>{data.type}</strong>
-                </div>
-              </div>
-            </div>
-          </div>);
-          })}
+        <div className="luxury_collection_content">
+          <div className="row">
+            {apartments.map((data) => {
+              if (data.type ==="LUXURY") {
+                return (
+                  <div className="col-sm-6" key={data.id}>
+                    {" "}
+                    <div className="card">
+                      <div className="card_img">
+                        <img src="./luxury-apartments.png" className="card-img-top" alt="..." />
+                      </div>
+                      <div className="card_body">
+                        <div className="like_button_luxury">
+                          <button>
+                            <FontAwesomeIcon
+                              icon={faHeart}
+                              beat
+                              className="highlight_luxury"
+                            />
+                          </button>
+                        </div>
+                        <div className="card_content_luxury">
+                          <h3>{data.name}</h3>
+                          <p>{data.description}</p>
+                          <Rate
+                            rating={rating2}
+                            onRating={(rate) => setRating2(rate)}
+                          />
+                          <strong>{data.pricePerNight}€</strong>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );  
+              }
+            })}
+          </div>
         </div>
-      </div>
       </div>
     </section>
   );
 }
 
-export default LuxuriousCollection
+export default LuxuriousCollection;
